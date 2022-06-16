9f99872ddc6e72aabbf21a37ebca38f3a8479f14
miriamdosantos <mii.santos@yahoo.com.br>
2022-06-16 21:36:42 +0100

    Implementação do Hilt no To Do

--- TodoAndroid/app/src/main/java/com/generation/todoandroid/model/Categoria.kt
+++
@@ -1,8 +0,0 @@
-package com.generation.todoandroid.model
-
-data class Categoria (
-    var id : Long,
-    var descricao: String,
-    var tarefa: List<Tarefa>,
-        ) {
-}
--- TodoAndroid/app/src/main/java/com/generation/todoandroid/util/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.generation.todoandroid.util
-
-object Constants {
-    const val BASE_URL = "https://todogenerationmobile.herokuapp.com/"
-}
